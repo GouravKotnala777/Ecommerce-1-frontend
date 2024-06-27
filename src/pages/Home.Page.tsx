ea0207f75790a3f2d987ef4388cebdb5057f9519
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-27 13:46:41 +0530

    feat : add home page

--- src/pages/Home.Page.tsx
+++ src/pages/Home.Page.tsx
@@ -1,54 +1,15 @@
-import { useEffect } from "react";
+import "../styles/pages/home.scss";
+import Products from "../components/Products";
+
 
 const Home = () => {
-
-
-    const aaGET = async() => {
-        console.log({SERVER_URL:import.meta.env.VITE_SERVER_URL});
-        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/v1/user/aa`, {
-            method:"GET",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            credentials:"include"
-        });
-
-        const data = await res.json();
-
-        console.log("----- Home.Page  aaGET");
-        console.log(data);
-        console.log("----- Home.Page  aaGET");
-        
-    };
-    const aaPOST = async() => {
-        console.log(`${import.meta.env.VITE_SERVER_URL}/api/v1/user/aa`);
-        
-        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/v1/user/aa`, {
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            credentials:"include"
-        });
-
-        const data = await res.json();
-
-        console.log("----- Home.Page  aaGET");
-        console.log(data);
-        console.log("----- Home.Page  aaGET");
-        
-    };
-
-
-    useEffect(() => {
-        aaGET();
-    }, []);
+    //import.meta.env.VITE_SERVER_URL
 
     return(
-        <>
+        <div className="home_bg">
             <h1>Home</h1>
-            <button onClick={aaPOST}>Fetch</button>
-        </>
+            <Products />
+        </div>
     )
 };
 
