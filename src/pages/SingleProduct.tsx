469644dfabe4ffda510140f85f1b5ff57134ae38
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-28 14:36:03 +0530

    refactor : add singleProductTemplate component in singleProduct page & style : update stylings

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,57 +1,15 @@
-import { BiHeart } from "react-icons/bi";
 import "../styles/pages/single_product.scss";
 import photo from "/vite.svg";
 import { useState } from "react";
 import { ProductTypes, productData } from "../assets/demoData";
+import SingleProductTemplate from "../components/SingleProductTemplate";
 
 const SingleProduct = () => {
     const [singleProduct, setSingleProduct] = useState<ProductTypes>(productData[0]);
 
     return(
         <div className="single_product_bg">
-            <div className="product_cont">
-                <div className="left_part">
-                    <img src={photo} alt={photo} />
-                </div>
-                <div className="right_part">
-                    <div className="whishlist_cont">
-                        <BiHeart className="BiHeart" />
-                        <span>Add to wishlist</span>
-                    </div>
-                    <div className="info_cont">
-                        <div className="heading_values">
-                            <span className="info_heading">Category</span><span className="info_value">{singleProduct.category}</span>
-                        </div>
-                        <div className="heading_values">
-                            <span className="info_heading">Name</span><span className="info_value">{singleProduct.name}</span>
-                        </div>
-                        <div className="heading_values">
-                            <span className="info_heading">Price</span><span className="info_value">{singleProduct.price}</span>
-                        </div>
-                        <div className="heading_values">
-                            <span className="info_heading">Rating</span><span className="info_value">{singleProduct.rating}</span>
-                        </div>
-                        <div className="heading_values">
-                            <span className="info_heading">Description</span><span className="info_value">{singleProduct.description}</span>
-                        </div>
-                    </div>
-                    <div className="btns_cont">
-                        <div className="upper_btns">
-                            <button className="add_btn">Add</button>
-                            <select>
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                            </select>
-                            <button className="buy_btn">Buy</button>
-                        </div>
-                        <div className="lower_btns">
-                            <button className="review_btn">Review</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <SingleProductTemplate category={singleProduct.category} name={singleProduct.name} price={singleProduct.price} rating={singleProduct.rating} description={singleProduct.description} photo={photo} parent="singleProduct" />
             
             <div className="reviews_cont">
                 {
