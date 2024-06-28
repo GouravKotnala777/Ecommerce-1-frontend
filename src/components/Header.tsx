45b912ca2a30bf9cc97dbdc72d064673e767f853
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-28 10:54:04 +0530

    feat : use 'isHamActive' redux state for show/hide sidebar & style : make header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,16 @@ import logo from "/public/vite.svg";
 import "../styles/components/header.scss";
 import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useState } from "react";
+import { setIsHamActive } from "../redux/reducers/miscReducers";
+import { useDispatch } from "react-redux";
 
 
 const Header = ({hideHeader}:{hideHeader:boolean}) => {
-    const [isSidebarActive, setIsSidebarActive] = useState<boolean>(false);
+    //const ssss = useSelector((state:{miscReducer:MiscReducerTypes}) => state.miscReducer);
+    const dispatch = useDispatch();
     //import.meta.env.VITE_SERVER_URL
+
+    
 
     return(
         <div className="header_bg" style={{top:hideHeader?"-12%":"-1%"}}>
@@ -26,7 +30,7 @@ const Header = ({hideHeader}:{hideHeader:boolean}) => {
 
 
 
-            <div className="left_part_mobile" onClick={() => setIsSidebarActive(true)}>
+            <div className="left_part_mobile" onClick={() => dispatch(setIsHamActive(true))}>
                 <GiHamburgerMenu className="GiHamburgerMenu" />
             </div>
             <div className="right_part_mobile">
