851b131ae250653a83f5d080babf534c974fc446
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-29 16:29:15 +0530

    feat : add useRegisterMutation reducer in store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import miscReducer from "./reducers/miscReducers";
+import api from "./api/api";
 
 
 export const store = configureStore({
     reducer:{
-        [miscReducer.name]:miscReducer.reducer
+        [miscReducer.name]:miscReducer.reducer,
+        [api.reducerPath]:api.reducer
     },
-    middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat()
+    middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware)
 });
