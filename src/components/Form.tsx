bf8f1d035e134988b3c21e9172700fefe7aa3b4a
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-30 12:20:42 +0530

    feat : add field type for textarea element

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,7 +4,7 @@ import "../styles/components/form.scss";
 interface FormPropTypes{
     heading:string;
     formFields:{type:string; name:string; placeHolder:string;}[];
-    onChangeHandler:(e:ChangeEvent<HTMLInputElement>) => void;
+    onChangeHandler:(e:ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) => void;
     onClickHandler:() => void;
 }
 
@@ -18,6 +18,9 @@ const Form = ({heading, formFields, onChangeHandler, onClickHandler}:FormPropTyp
             <div className="fields_cont">
                 {
                     formFields.map((field, index) => (
+                        field.type === "textArea" ?
+                        <textarea key={index} rows={5} name={field.name} placeholder={field.placeHolder+"..."} onChange={(e) => onChangeHandler(e)} />
+                        :
                         <input key={index} type={field.type} name={field.name} placeholder={field.placeHolder} onChange={(e) => onChangeHandler(e)} />
                     ))
                 }
