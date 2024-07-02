d7d866e158c7264cf569b077a0689ea5d3ef3604
gouravkotnala <gouravkotnala777@gmail.com>
2024-07-02 12:52:09 +0530

    style(Home.Page.tsx) : remove unnecessary lines

--- src/pages/Home.Page.tsx
+++ src/pages/Home.Page.tsx
@@ -3,8 +3,6 @@ import Products from "../components/Products";
 
 
 const Home = () => {
-    //import.meta.env.VITE_SERVER_URL
-
     return(
         <div className="home_bg">
             <h1>Home</h1>
