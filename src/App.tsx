db5f099c21a6a24011e60ff258b074d033e5bf9f
gouravkotnala <gouravkotnala777@gmail.com>
2024-07-12 11:59:40 +0530

    feat(App.tsx) : add routers for Payment.tsx and Address.tsx page & feat : store loggedInUser data in redux

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from './pages/Home.Page'
 import Header from './components/Header'
 import { useEffect } from 'react'
 import Sidebar from './components/Sidebar'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { MiscReducerTypes } from './redux/reducers/miscReducers'
 import SingleProduct from './pages/SingleProduct'
 import Cart from './pages/Cart.Page'
@@ -17,14 +17,20 @@ import Wishlist from './pages/Wishlist'
 import Dashboard from './pages/Dashboard'
 import OutStock from './pages/admin/OutStock'
 import UpdateProduct from './pages/admin/UpdateProduct'
-import Discount from './pages/admin/Discount'
+//import Discount from './pages/admin/Discount'
 import Coupons from './pages/admin/Coupon'
+import StripePayment from './pages/Payment'
+import Address from './pages/Address.Page'
+import { useMyProfileQuery } from './redux/api/api'
+import { UserTypes } from './assets/demoData'
+import { setLoggedInUser } from './redux/reducers/loggedInUserReducer'
 
 
 
 const App = () => {
   const {isHamActive} = useSelector((state:{miscReducer:MiscReducerTypes}) => state.miscReducer);
-
+  const myProfileData:{data?:{message:UserTypes}} = useMyProfileQuery("");
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (isHamActive) {
@@ -36,12 +42,17 @@ const App = () => {
 
   }, [isHamActive]);
   
+  useEffect(() => {
+    dispatch(setLoggedInUser({user:myProfileData.data?.message as UserTypes, isLoading:false, isError:false}));
+  }, [myProfileData.data?.message]);
+  
 
   return (
     <>
       <BrowserRouter>
         <Header />
         <Sidebar />
+        {/*<pre>{JSON.stringify(myProfileData.data?.message, null, `\t`)}</pre>*/}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product/new" element={<AddProduct />} />
@@ -49,8 +60,10 @@ const App = () => {
           <Route path="/user/login" element={<Login />} />
           <Route path="/user/logout" element={<Logout />} />
           <Route path="/user/cart" element={<Cart />} />
+          <Route path="/product/pay" element={<StripePayment />} />
           <Route path="/product/:productID" element={<SingleProduct />} />
           <Route path="/user/wishlist" element={<Wishlist />} />
+          <Route path="/user/address" element={<Address />} />
           <Route path="/admin/dashboard" element={<Dashboard />} />
           <Route path="/admin/outstock" element={<OutStock />} />
           <Route path="/admin/product/update" element={<UpdateProduct />} />
