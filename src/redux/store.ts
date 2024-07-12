e4ff5fec857436e29dcc77d159b0714bfa92a2bd
gouravkotnala <gouravkotnala777@gmail.com>
2024-07-12 11:25:18 +0530

    feat(store.ts) : add loggedInUserReducer in store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import miscReducer from "./reducers/miscReducers";
 import api from "./api/api";
+import loggedInUserReducer from "./reducers/loggedInUserReducer";
 
 
 export const store = configureStore({
     reducer:{
         [miscReducer.name]:miscReducer.reducer,
+        [loggedInUserReducer.name]:loggedInUserReducer.reducer,
         [api.reducerPath]:api.reducer
     },
     middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware)
