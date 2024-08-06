5ccb5d8b5a81d80f794d6adfb4f1adc6b31fd073
gouravkotnala <gouravkotnala777@gmail.com>
2024-08-06 16:38:20 +0530

    style(Spinner.tsx) : add styles

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -14,13 +14,15 @@ const Spinner = ({heading, width, color, thickness, type}:SpinnerPropTypes) => {
     return(
         <div className="spinner_cont" style={{width:width?`${width}px`:"15px", height:width?`${width}px`:"15px"}}>
             <div className="spinner" style={{
-                borderColor:color?`${color}`:"rgb(255, 75, 105)",
+                borderColor:color?color:"rgb(255, 75, 105)",
                 borderWidth:thickness?thickness:"2px",
+                //borderStyle:"solid",
 
-                borderLeftWidth:thickness?thickness:"2px",
                 borderLeftColor:"transparent",
+                borderLeftWidth:thickness?thickness:"2px",
+                //borderLeftStyle:"solid",
 
-                borderRightColor:type===1?"transparent":"rgb(255, 75, 105)",
+                borderRightColor:type===1?"transparent":color?color:"rgb(255, 75, 105)",
                 borderRightWidth:thickness?thickness:"2px",
             }}>
 
