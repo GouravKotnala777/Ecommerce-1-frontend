8e1d773baf7e81d2cdd7c5698e262b9fc32c5a46
gouravkotnala <gouravkotnala777@gmail.com>
2024-08-07 16:29:51 +0530

    feat(ItemNotFound.tsx) : add component for item not found

---
+++ src/components/ItemNotFound.tsx
@@ -0,0 +1,15 @@
+import "../styles/components/item_not_found.scss";
+
+const ItemNotFound = ({heading, statusCode}:{heading:string; statusCode:number;}) => {
+
+    return(
+        <div className="item_not_found_outer_cont">
+        <div className="item_not_found_inner_cont">
+            <div className="statusCode">{statusCode}</div>
+            <div className="heading">{heading}</div>
+        </div>
+        </div>
+    )
+}
+
+export default ItemNotFound;
