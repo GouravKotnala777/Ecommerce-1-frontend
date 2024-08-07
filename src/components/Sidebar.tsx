bc287ba6f47fd9cace3b680cf3f893bc63ec3428
gouravkotnala <gouravkotnala777@gmail.com>
2024-08-07 10:59:05 +0530

    feat(Sidebar.tsx) : close sidebar after selecting of any router link from it

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,22 +9,26 @@ const Sidebar = () => {
     const {isHamActive} = useSelector((state:{miscReducer:MiscReducerTypes}) => state.miscReducer);
     const dispatch = useDispatch();
 
+    const closeSidebarHandler = () => {
+        dispatch(setIsHamActive(false));
+    };
+
     
     return(
         <div className="sidebar_bg" style={{left:isHamActive?"0%":"-100%"}}>
             <div className="left_part">
                 <div className="scrollable_part">
-                    <NavLink className="navlink" to="/">Home</NavLink>
-                    <NavLink className="navlink" to="/user/register">Register</NavLink>
-                    <NavLink className="navlink" to="/user/login">Login</NavLink>
-                    <NavLink className="navlink" to="/product/new">Add Product</NavLink>
-                    <NavLink className="navlink" to="/user/wishlist">Wishlist</NavLink>
-                    <NavLink className="navlink" to="/admin/dashboard">Dashboard</NavLink>
-                    <NavLink className="navlink" to="/user/cart">Cart</NavLink>
-                    <NavLink className="navlink" to="/user/logout">Logout</NavLink>
+                    <NavLink className="navlink" to="/" onClick={closeSidebarHandler}>Home</NavLink>
+                    <NavLink className="navlink" to="/user/register" onClick={closeSidebarHandler}>Register</NavLink>
+                    <NavLink className="navlink" to="/user/login" onClick={closeSidebarHandler}>Login</NavLink>
+                    <NavLink className="navlink" to="/product/new" onClick={closeSidebarHandler}>Add Product</NavLink>
+                    <NavLink className="navlink" to="/user/wishlist" onClick={closeSidebarHandler}>Wishlist</NavLink>
+                    <NavLink className="navlink" to="/admin/dashboard" onClick={closeSidebarHandler}>Dashboard</NavLink>
+                    <NavLink className="navlink" to="/user/cart" onClick={closeSidebarHandler}>Cart</NavLink>
+                    <NavLink className="navlink" to="/user/logout" onClick={closeSidebarHandler}>Logout</NavLink>
                 </div>
             </div>
-            <div className="right_part" onClick={() => dispatch(setIsHamActive(false))}>
+            <div className="right_part" onClick={closeSidebarHandler}>
                 <CgClose className="CgClose" />
             </div>
         </div>
