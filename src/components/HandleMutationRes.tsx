058864022840aa8fee135c2978a10930cf466cb3
gouravkotnala <gouravkotnala777@gmail.com>
2024-08-08 10:49:41 +0530

    fix(HandleMutationRes.tsx) : place code inside useEffect hook

--- src/components/HandleMutationRes.tsx
+++ src/components/HandleMutationRes.tsx
@@ -1,5 +1,5 @@
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
-import { memo } from "react";
+import { memo, useEffect } from "react";
 import toast, {Toaster} from "react-hot-toast";
 import { MutationResTypes } from "../assets/demoData";
 import { useNavigate } from "react-router-dom";
@@ -8,37 +8,41 @@ import { useNavigate } from "react-router-dom";
 
 const HandleMutationRes = memo(({res, redirect}:{res:MutationResTypes|undefined; redirect?:string;}) => {
     const navigate = useNavigate();
-    
-    if (res?.error) {
-        const error = res?.error as FetchBaseQueryError;
-        if (error.data && typeof error.data === "object" && "message" in error.data) {
-            console.log(res?.error);
-            toast.error(error.data.message as string, {
-                position:"bottom-center",
-                duration:2000
-            })
-        }
-    }
-    else if (res?.data) {
-        if (res?.data.success) {
-            toast.success(res?.data.message, {
-                position:"bottom-center",
-                duration:2000
-            });
 
-            if (redirect) {
-                setTimeout(() => {
-                    navigate(redirect);
-                }, 1500);
+
+
+    useEffect(() => {        
+        if (res?.error) {
+            const error = res?.error as FetchBaseQueryError;
+            if (error.data && typeof error.data === "object" && "message" in error.data) {
+                console.log(res?.error);
+                toast.error(error.data.message as string, {
+                    position:"bottom-center",
+                    duration:2000
+                })
             }
         }
-        else{
-            toast.error(res.data.message, {
-                position:"bottom-center",
-                duration:2000,
-            })
+        else if (res?.data) {
+            if (res?.data.success) {
+                toast.success(res?.data.message, {
+                    position:"bottom-center",
+                    duration:2000
+                });
+    
+                if (redirect) {
+                    setTimeout(() => {
+                        navigate(redirect);
+                    }, 1500);
+                }
+            }
+            else{
+                toast.error(res.data.message, {
+                    position:"bottom-center",
+                    duration:2000,
+                })
+            }
         }
-    }
+    }, [res]);
 
     return(
         <>
