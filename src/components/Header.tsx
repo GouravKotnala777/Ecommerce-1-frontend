f155c2db6dfbeb01fb11ba9b1170c10b7e86b835
gouravkotnala <gouravkotnala777@gmail.com>
2024-08-18 16:05:45 +0530

    feat(Header.tsx) : add preview for update myProfile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import Sidebar from "./Sidebar";
 import DialogWrapper from "./DialogWrapper";
 import { useSelector } from "react-redux";
 import { loggedInUserInitialState } from "../redux/reducers/loggedInUserReducer";
-import { BiEdit } from "react-icons/bi";
+import { BiEdit, BiRightArrowAlt } from "react-icons/bi";
 import { CgClose } from "react-icons/cg";
 import { useUpdateMeMutation } from "../redux/api/api";
 //import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
@@ -128,41 +128,61 @@ const MyProfileDialog = ({setIsMyProfileDialogOpen}:{setIsMyProfileDialogOpen:Di
     };
     
     return(
-        <div className="my_profile_dialog_cont" onClick={(e) => e.stopPropagation()}>
-            <div className="main_heading">My Profile</div>
-            <p style={{fontSize:"0.6rem"}}>{JSON.stringify(updateFormData)}</p>
+        <div className="my_profile_dialog_cont">
             {
-                isLoading ?
+                updateFormData.name || updateFormData.mobile || updateFormData.password ?
+                    <div className="update_preview_cont">
+                        <div className="main_heading">Update Preview</div>
+                        <div className="my_details">
+                            <div className="heading">Name</div><div className="value">{user?.name}</div><BiRightArrowAlt /><div className="value">{updateFormData.name}</div>
+                            <div className="heading">Password</div><div className="value">*********</div><BiRightArrowAlt /><div className="value">{updateFormData.password}</div>
+                            <div className="heading">Mobile</div><div className="value">{user?.mobile}</div><BiRightArrowAlt /><div className="value">{updateFormData.mobile}</div>
+                        </div>
+                    </div>
+                    :
+                    <></>
+            }
+            <div className="my_profile_cont" onClick={(e) => e.stopPropagation()}>
+                <div className="main_heading">My Profile</div>
+                {/*<pre style={{fontSize:"0.6rem"}}>{JSON.stringify(updateFormData, null, `\t`)}</pre>*/}
+                {
+                    isLoading ?
                     <h1>Loading...</h1>
                     :
                     <>
-                        <div className="my_details">
-                            <div className="heading">Name</div><div className="value">{user?.name} <input type="text" name="name" id="name" placeholder="New Name" value={updateFormData?.name ? updateFormData?.name : ""} onChange={(e) => updateFormChangeHandler(e)} style={{display:editFieldList.includes("name")?"block":"none"}} /> {editFieldList.includes("name") ? <CgClose id="name-CgClose" className="BiEdit" onClick={(e) => func(e)}  /> : <BiEdit id="name-BiEdit" className="BiEdit" onClick={(e) => func(e)} />}</div>
-                            <div className="heading">Email</div><div className="value">{user?.email}</div>
-                            <div className="heading">Password</div><div className="value">********** <input type="text" name="password" id="password" placeholder="New Password" value={updateFormData?.password ? updateFormData?.password : ""} onChange={(e) => updateFormChangeHandler(e)} style={{display:editFieldList.includes("password")?"block":"none"}} /> {editFieldList.includes("password") ? <CgClose id="password-CgClose" className="BiEdit" onClick={(e) => func(e)}  /> : <BiEdit id="password-BiEdit" className="BiEdit" onClick={(e) => func(e)} />}</div>
-                            <div className="heading">Mobile</div><div className="value">{user?.mobile} <input type="text" name="mobile" id="mobile" placeholder="New Mobile" value={updateFormData?.mobile ? updateFormData?.mobile : ""} onChange={(e) => updateFormChangeHandler(e)} style={{display:editFieldList.includes("mobile")?"block":"none"}} /> {editFieldList.includes("mobile") ? <CgClose id="mobile-CgClose" className="BiEdit" onClick={(e) => func(e)}  /> : <BiEdit id="mobile-BiEdit" className="BiEdit" onClick={(e) => func(e)} />}</div>
-                            <div className="heading">Role</div><div className="value">{user?.role}</div>
-                            {
-                                editFieldList.length !== 0 &&
+                            <div className="my_details">
+                                <div className="heading">Name</div>
+                                    <div className="value">
+                                        {!editFieldList.includes("name")&&user?.name}
+                                        <input type="text" name="name" id="name" placeholder="New Name" value={updateFormData?.name ? updateFormData?.name : ""} onChange={(e) => updateFormChangeHandler(e)} style={{display:editFieldList.includes("name")?"block":"none"}} />
+                                        {editFieldList.includes("name") ? <CgClose id="name-CgClose" className="BiEdit" onClick={(e) => func(e)}  /> : <BiEdit id="name-BiEdit" className="BiEdit" onClick={(e) => func(e)} />}
+                                    </div>
+                                <div className="heading">Email</div><div className="value">{user?.email}</div>
+                                <div className="heading">Password</div><div className="value">{!editFieldList.includes("password")&&"**********"} <input type="text" name="password" id="password" placeholder="New Password" value={updateFormData?.password ? updateFormData?.password : ""} onChange={(e) => updateFormChangeHandler(e)} style={{display:editFieldList.includes("password")?"block":"none"}} /> {editFieldList.includes("password") ? <CgClose id="password-CgClose" className="BiEdit" onClick={(e) => func(e)}  /> : <BiEdit id="password-BiEdit" className="BiEdit" onClick={(e) => func(e)} />}</div>
+                                <div className="heading">Mobile</div><div className="value">{!editFieldList.includes("mobile")&&user?.mobile} <input type="text" name="mobile" id="mobile" placeholder="New Mobile" value={updateFormData?.mobile ? updateFormData?.mobile : ""} onChange={(e) => updateFormChangeHandler(e)} style={{display:editFieldList.includes("mobile")?"block":"none"}} /> {editFieldList.includes("mobile") ? <CgClose id="mobile-CgClose" className="BiEdit" onClick={(e) => func(e)}  /> : <BiEdit id="mobile-BiEdit" className="BiEdit" onClick={(e) => func(e)} />}</div>
+                                <div className="heading">Role</div><div className="value">{user?.role}</div>
+                                {
+                                    editFieldList.length !== 0 &&
                                     <>
-                                        <div className="heading">Old Password</div><div className="value"><input type="text" name="oldPassword" placeholder="Old Password" value={updateFormData?.oldPassword ? updateFormData?.oldPassword : ""} onChange={(e) => updateFormChangeHandler(e)} /></div>
-                                    </>
+                                            <div className="heading">Old Password</div><div className="value"><input type="text" name="oldPassword" placeholder="Old Password" value={updateFormData?.oldPassword ? updateFormData?.oldPassword : ""} onChange={(e) => updateFormChangeHandler(e)} /></div>
+                                        </>
+                                }
+                            </div>
+                            {
+                                editFieldList.length === 0 ?
+                                    <div className="btns">
+                                        <span></span>
+                                        <button className="close_btn" onClick={myProfileDialogCloseHandler}>Close</button>
+                                    </div>
+                                    :
+                                    <div className="btns">
+                                        <button className="close_btn" onClick={myProfileDialogCloseHandler}>Cancel</button>
+                                        <button className="update_btn" onClick={updateMeHandler}>Update</button>
+                                    </div>
                             }
-                        </div>
-                        {
-                            editFieldList.length === 0 ?
-                                <div className="btns">
-                                    <span></span>
-                                    <button className="close_btn" onClick={myProfileDialogCloseHandler}>Close</button>
-                                </div>
-                                :
-                                <div className="btns">
-                                    <button className="close_btn" onClick={myProfileDialogCloseHandler}>Cancel</button>
-                                    <button className="update_btn" onClick={updateMeHandler}>Update</button>
-                                </div>
-                        }
-                    </>
-            }
+                        </>
+                }
+            </div>
         </div>
     )
 }
