da931c3cd372fe064a4452ebd2e11dc1e4880bde
gouravkotnala <gouravkotnala777@gmail.com>
2024-09-30 11:25:11 +0530

    refactor(Tab.tsx) : initialize initial value for tabs

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -8,17 +8,18 @@ interface PaneslArrayTypes{
 }
 
 const Tab = ({panelsArray}:{panelsArray:PaneslArrayTypes[]}) => {
-    const [selectedTab, setSelectedTab] = useState<string>("");
+    const [selectedTab, setSelectedTab] = useState<string>(panelsArray[0].name);
 
     return(
         <div className="tab_panel">
             <div className="tabs_cont">
                 {
                     panelsArray.map((item) => (
+                            
                         <button className="tab" value={item.name} style={{
                             background:item.name === selectedTab ? `linear-gradient(90deg, ${PRIMARY}, ${SECONDARY})` : "white",
-                            color:item.name === selectedTab ? "white" : "black",
-                            boxShadow:item.name === selectedTab ? "0px 0px 3px 1px rgba(0,0,0,0.3) inset" : "0px 0px 5px 1px rgba(0,0,0,0.3)"
+                            color:item.name === selectedTab ? "white" : "#515151",
+                            boxShadow:item.name === selectedTab ? "0px 0px 3px 1px rgba(0,0,0,0.3) inset" : "0px 0px 5px 1px rgba(0,0,0,0.1)"
                         }} onClick={(e) => setSelectedTab(e.currentTarget.value)}>{item.name}</button>
                     ))
                 }
