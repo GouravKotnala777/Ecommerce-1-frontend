403fbe06324b0eb69d0543d33c035dd4a21b7da6
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-05 18:44:49 +0530

    feat(ShareButton.tsx) : add a button component for sharing referral link

---
+++ src/components/ShareButton.tsx
@@ -0,0 +1,32 @@
+import "../styles/components/share_btn.scss";
+import { MdOutlineShare } from "react-icons/md";
+
+
+const ShareButton = ({placeHolder, title, text, url}:{placeHolder?:string; title:string; text:string; url:string;}) => {
+    const shareHandler = async() => {
+        if (navigator.share) {
+            try {
+                console.log("------ ShareButton");
+                await navigator.share({
+                    title, text, url
+                });
+                console.log("------ ShareButton");
+            } catch (error) {
+                console.log("------ ShareButton error");
+                console.log(error);
+                console.log("------ ShareButton error");
+            }
+        }
+        else{
+            console.log("Web Share api is not supported in your browser");
+        }
+    }
+
+    return(
+        <button className="share_btn" onClick={shareHandler}>
+            <MdOutlineShare className="MdOutlineShare" /> {placeHolder}
+        </button>
+    )
+}
+
+export default ShareButton;
